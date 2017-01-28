64d4aae1e630e70e63e91e4dc55439a978edd846
cesar <yulio3726@hotmail.com>
2017-01-28 13:57:36 -0800

    antes de los cambios para las pruebas mas rapidas

---
+++ rbbf1/cmake-build-debug/CMakeFiles/rbbf1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rbbf1.dir/rbbf1.c.o"
+  "rbbf1.pdb"
+  "rbbf1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/rbbf1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ rbbf1/cmake-build-debug/CMakeFiles/rbbf1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for rbbf1.
+# This may be replaced when dependencies are built.
